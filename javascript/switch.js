d9f34c643dd7f4df308dd47231fe2a5d15e5f49a
javaherobrine <620009897@qq.com>
2020-04-07 17:08:59 +0800

    修复bug

--- javascript/switch.js
+++ javascript/switch.js
@@ -3,14 +3,14 @@ function output(arg1,arg2,arg3){
 	var v2;
 	v="type:"+arg1;
 	v2=document.createElement("p");
-	document.body.appendChild(v2);
+	v2=document.body.appendChild(v2);
 	v2.innerHTML=v;
 	v="main-class:"+arg2;
 	v2=document.createElement("p");
-	document.body.appendChild(v2);
+	v2=document.body.appendChild(v2);
 	v2.innerHTML=v;
 	v="main-method:"+arg3;
 	v2=document.createElement("p");
-	document.body.appendChild(v2);
+	v2=document.body.appendChild(v2);
 	v2.innerHTML=v;
 }
