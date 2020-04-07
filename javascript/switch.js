de03bc2545a338d579be269aa37c9f9d80ed5557
javaherobrine <620009897@qq.com>
2020-04-07 15:32:35 +0800

    q

--- javascript/switch.js
+++ javascript/switch.js
@@ -3,13 +3,13 @@ function output(){
 	var v2;
 	v2=document.createElement("p");
 	document.body.appendChild(v2);
-	v2.innerHTML=v;
+	v2.innerText=v;
 	v="main-class:"+document.getElementById("class").value;
 	v2=document.createElement("p");
 	document.body.appendChild(v2);
-	v2.innerHTML=v;
+	v2.innerText=v;
 	v="main-method:"+document.getElementById("method").value;
 	v2=document.createElement("p");
 	document.body.appendChild(v2);
-	v2.innerHTML=v;
+	v2.innerText=v;
 }
