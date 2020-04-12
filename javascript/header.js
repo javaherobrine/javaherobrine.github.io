f800eb940309c0ad4d54b1e5410b424a02c94957
javaherobrine <620009897@qq.com>
2020-04-12 09:03:48 +0800

    再度修bug

--- javascript/header.js
+++ javascript/header.js
@@ -2,7 +2,7 @@ var temp=document.getElementById("header");
 var temp2=document.createElement("button");
 temp2=temp.appendChild(temp2);
 temp2.addEventListener("click",function(){
-	window.location.href="../index.html";
+	window.location.href="index.html";
 },true);
 temp2.innerHTML="首页";
 temp2=document.createElement("button");
@@ -13,19 +13,19 @@ temp2.addEventListener("click",function(){
 	}else{
 		switch(v){
 			case "zh-cn":
-				window.location.href=goTo()+"zh-cn";
+				window.location.href=goTo()+"/zh-cn";
 				break;
 			case "en-us":
-				window.location.href=goTo()+"en-us";
+				window.location.href=goTo()+"/en-us";
 				break;
 			default:
-				window.location.href=goTo()+"choose.html";
+				window.location.href=goTo()+"/choose.html";
 				break;
 		}
 	}
 },true);
 temp2=temp.appendChild(temp2);
-temp.innerHTML="下载";
+temp2.innerHTML="下载";
 function goTo(){
 	var strFullPath=window.document.location.href;
 	var strPath=window.document.location.pathname;
